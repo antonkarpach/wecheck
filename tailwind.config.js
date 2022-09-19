3e6aa48489a70e848cb9fe640db4d424c4f23bab
Anton Karpach <karpach.anton@gmail.com>
2022-09-19 05:20:24 +0300

    Add main structure with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0px 2px 8px rgba(0, 0, 0, 0.1)',
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    function ({ addVariant }) {
+      addVariant('child', '& > *');
+      addVariant('child-hover', '& > *:hover');
+    }
+  ],
+}
